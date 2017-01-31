31b391373674b19606a303f9bcceead97d56c433
Kabir M <mgkabir@gmail.com>
2017-02-01 01:48:52 +1100

    login works

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,22 +1,39 @@
 import { Injectable } from '@angular/core';
-
+import { Http, Response, Headers} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/delay';
 
+import {LoginInfo} from './login.component';
+
 @Injectable()
 export class AuthService {
   isLoggedIn: boolean = false;
-
+  private baseUrl:string = 'http://localhost:8080';
+  constructor(private http:Http){
+    }
   // store the URL so we can redirect after logging in
   redirectUrl: string;
 
-  login(): Observable<boolean> {
-    return Observable.of(true).delay(2000).do(val => this.isLoggedIn = true);
+  login(loginInfo: LoginInfo): Observable<boolean> {
+    return this.http
+      .post(`${this.baseUrl}/checklogin`,JSON.stringify(loginInfo), {headers: this.getContentTypeHeader()})
+      .map((res:Response)=>{
+        console.log(`AuthService.login() : Response JSON : ${res.json()}`);
+        this.isLoggedIn = res.json();
+        return res.json();
+      });
+    //return Observable.of(true).delay(2000).do(val => this.isLoggedIn = true);
   }
 
   logout(): void {
     this.isLoggedIn = false;
+  }
+
+  private getContentTypeHeader(){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return headers;
   }
 }
--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -4,28 +4,50 @@ import { AuthService } from './auth.service';
 
 @Component({
   template: `
-    <div class="well">
+    <div class="well container">
       <h4>Login Page</h4>
       <p class="text-info">{{message}}</p>
-      <p>
-        <button (click)="login()"  *ngIf="!authService.isLoggedIn" class="btn btn-default">Login</button>
-        <button (click)="logout()" *ngIf="authService.isLoggedIn" class="btn btn-default">Logout</button>
-      </p>
+      <form #f="ngForm">
+        <div class="row">
+          <div class="form-group">
+              <label for="userName">User Name {{loginInfo.userName}}</label>
+              <input type="text" [(ngModel)]="loginInfo.userName" name="userName" id="userName" class="form-control" placeholder="User Name" required>
+          </div>
+        </div>
+        <div class="row">
+          <div class="form-group">
+              <label for="password">Password {{loginInfo.password}}</label>
+              <input type="password" [(ngModel)]="loginInfo.password" name="password" id="password" class="form-control" placeholder="Password" required>
+          </div>
+        </div>
+        <div class="row">
+          <button (click)="login()"  *ngIf="!authService.isLoggedIn" class="btn btn-default">Login</button>
+          <button (click)="logout()" *ngIf="authService.isLoggedIn" class="btn btn-default">Logout</button>
+
+        </div>
+      </form>
     </div>
     `
 })
 
 export class LoginComponent {
   message: string;
+  loginInfo: LoginInfo = {
+    userName:"",
+    password:""
+  };
+
   constructor(public authService: AuthService, public router: Router) {
     this.setMessage();
   }
+
   setMessage() {
     this.message = 'You are Logged ' + (this.authService.isLoggedIn ? 'In' : 'Out');
   }
+
   login() {
     this.message = 'Trying to log In ...';
-    this.authService.login().subscribe(() => {
+    this.authService.login(this.loginInfo).subscribe(() => {
       this.setMessage();
       if (this.authService.isLoggedIn) {
         // Get the redirect URL from our auth service
@@ -36,8 +58,14 @@ export class LoginComponent {
       }
     });
   }
+
   logout() {
     this.authService.logout();
     this.setMessage();
   }
+}
+
+export class LoginInfo {
+  userName: string;
+  password: string;
 }
