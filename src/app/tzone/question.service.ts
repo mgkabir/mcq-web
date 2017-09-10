f976ae4063196860684ac23fba0026c8bc6490ec
Mohammad Kabir <mgkabir@gmail.com>
2017-09-10 22:09:22 +1000

    refactor

--- src/app/tzone/question.service.ts
+++ src/app/tzone/question.service.ts
@@ -2,15 +2,13 @@ import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Response, Headers} from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { Question } from './question';
-import {AuthService} from '../auth.service';
 
 @Injectable()
 export class QuestionService{
 
   private baseUrl:string = 'http://localhost:8080';
   //private baseUrl:string = 'http://ec2-52-62-233-77.ap-southeast-2.compute.amazonaws.com:8080';
-  constructor(private http:Http, private authService:AuthService){
-    }
+  constructor(private http:Http){}
 
     /* practice-question is accessible without any token*/
     getRandomQuestion(): Observable<Question>{
